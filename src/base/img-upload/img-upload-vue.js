b774076a002c16200080b2a0a5ac50ea34625b8b
awoo <awoooo@me.com>
2018-03-09 15:40:10 +0800

    add img upload comp

---
+++ src/base/img-upload/img-upload-vue.js
@@ -0,0 +1,68 @@
+exports.js = () => {
+  return {
+    name: 'img-upload',
+    components: {
+    },
+
+    created () {
+    },
+
+    props: {
+      multilple: {
+        type: Boolean,
+        default: false
+      }
+    },
+
+    data () {
+      return {
+        imgs: []
+      }
+    },
+
+    computed: {
+    },
+
+    watch: {
+    },
+
+    methods: {
+      getFiles (event) {
+        let method = this.multilple ? 'getImgs' : 'getImg'
+        this[method](event)
+        this.emit(event.target.files)
+      },
+
+      getImg (event) {
+        this.file = event.target.files[0]
+        if (!this.file || !window.FileReader) return
+        if (!/^image/.test(this.file.type)) return
+        setImgs(this.imgs, this.file)
+      },
+
+      getImgs (event) {
+        this.files = event.target.files
+        if (!this.files || !window.FileReader) return
+        for (let i = 0; i < this.files.length; i++) {
+          if (!/^image/.test(this.files[i].type)) { continue }
+          setImgs(this.imgs, this.files[i])
+        }
+      },
+
+      dispatch () { this.$refs.file.click() },
+      emit (val) { this.$emit('getFiles', event.target.files) }
+    },
+
+    mounted () {
+    }
+  }
+}
+
+function setImgs (imgs, file) {
+  this.imgs = []
+  let reader = new FileReader()
+  reader.readAsDataURL(file)
+  reader.onloadend = () => {
+    imgs.push(reader.result)
+  }
+}
---
+++ src/base/img-upload/img-upload.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .img-upload
+    input.upload(
+      ref="file"
+      type="file"
+      @change="getFiles($event)"
+      multiple="multiple"
+      )
+    .mdi.mdi-upload(@click="dispatch")
+      span image
+    .preview(v-show="imgs.length > 0")
+      img(v-for="img in imgs" :src="img")
+</template>
+
+<script type="text/ecmascript-6">
+  import { js } from './img-upload-vue.js'
+  export default js.call(this)
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import '~common/stylus/variable.styl'
+  @import './img-upload.styl'
+.img-upload
+  .upload
+    display none
+  .mdi
+    cursor pointer
+  .preview
+  img
+    height 3rem
+    width auto
+</style>
