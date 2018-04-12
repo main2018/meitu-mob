0c7ffcaa6092376c551201b1c458abba0a34b9c9
awoo <awoooo@me.com>
2018-04-12 15:47:30 +0800

    add card-input

---
+++ src/admin/card-input/card-input-vue.js
@@ -0,0 +1,79 @@
+exports.js = () => {
+  const { qiniuUpload, qiniuDel } = require('common/js/qiniu-api.js')
+  const { QINIU_URL_PREFIX } = require('config')
+  const { setObjectPropToData } = require('common/js')
+  return {
+    name: 'card-input',
+    components: {
+    },
+
+    created () {
+    },
+
+    props: {
+      content: {
+        type: Object,
+        default: () => {}
+      },
+      title: {
+        type: Number,
+        default: 0
+      }
+    },
+
+    data () {
+      return {
+        http: QINIU_URL_PREFIX,
+        card: {
+          status: 0,
+          cover: '',
+          title: 'haha',
+          desc: ''
+        }
+      }
+    },
+
+    computed: {
+      coverStyle () {
+        let cover = this.card.cover
+        let pre = 'background-image: url('
+        return cover ? `${pre}${this.http}${cover});` : ''
+      }
+    },
+
+    watch: {
+      content () {
+        setObjectPropToData.call(this, 'content', 'card')
+        this.getProps()
+      }
+    },
+
+    methods: {
+      getFiles (event) {
+        let { cover } = this.card
+        let file = event.target.files[0]
+        cover && qiniuDel(cover)
+        qiniuUpload(file, fname => {
+          this.card.cover = fname
+          this.changed()
+        })
+      },
+      getProps () {
+        let ob = {}
+        for (let key in this.card) {
+          ob[key] = this.card[key]
+        }
+        this.card = this.content
+        for (let key in ob) {
+          let hasKey = this.card.hasOwnProperty(key)
+          if (!hasKey) { this.card[key] = ob[key] }
+        }
+      },
+      dispatch () { this.$refs.file.click() },
+      changed () { this.$emit('changed', this.card) }
+    },
+
+    mounted () {
+    }
+  }
+}
---
+++ src/admin/card-input/card-input.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  .card-input
+    .input-cover
+      .upload(@click="dispatch")
+        .mdi.mdi-upload cover
+      .preview(:style="coverStyle")
+    .input-list
+      ul.input-item
+        li
+          label * title
+          input(type="text" @change="changed" v-model="card.title")
+        li
+          label * desc
+          textarea(@change="changed" v-model="card.desc")
+      .input-options
+        span.hint status
+        span.radio
+          label online &nbsp;
+            input(
+              type="radio"
+              name="status"
+              checked="checked"
+              value="0"
+              v-model="card.status"
+              @change="changed"
+              )
+          label outline &nbsp;
+            input(
+              type="radio"
+              name="status"
+              value="1"
+              v-model="card.status"
+              @change="changed"
+              )
+
+    input(
+      ref="file"
+      type="file"
+      accept="image/*"
+      @change="getFiles($event)"
+      style="display: none; z-index: -1;"
+        )
+</template>
+
+<script type="text/ecmascript-6">
+  import { js } from './card-input-vue.js'
+  export default js.call(this)
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import '~common/stylus/variable.styl'
+  @import './card-input.styl'
+</style>
